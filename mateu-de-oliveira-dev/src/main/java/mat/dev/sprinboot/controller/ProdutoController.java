8c53df2493c11bc53203a92f95b47efdd9860f45
alex_ <alex_@DESKTOP-CBITOJH>
2026-04-22 19:54:44 -0300

    dd

--- mateu-de-oliveira-dev/src/main/java/mat/dev/sprinboot/controller/ProdutoController.java
+++ mateu-de-oliveira-dev/src/main/java/mat/dev/sprinboot/controller/ProdutoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,7 @@ public class ProdutoController {
 		return ResponseEntity.ok(prodSaldo);
 	}
 
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public ResponseEntity<Produto> update(@RequestBody Produto produto) throws Exception {
 
 		Optional<Produto> optional = produtoService.existe(produto.getId());
@@ -114,6 +115,7 @@ public class ProdutoController {
 		return ResponseEntity.ok().build();
 		
 	}
+	
 	
 
 }
